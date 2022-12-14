6a4e5c92863f8880df4f5fff9a3583dff9a722b0
jmmalunao <jmmalunao@gmail.com>
2022-12-14 17:53:27 +0800

    ss

--- src/api/axios.js
+++ src/api/axios.js
@@ -5,11 +5,9 @@ const API_URL = process.env.REACT_APP_API_URL
 export default axios.create({
   baseURL: API_URL,
   headers: { "Content-Type": "application/json" },
-  withCredentials: true,
 })
 
 export const axiosPrivate = axios.create({
   baseURL: API_URL,
   headers: { "Content-Type": "application/json" },
-  withCredentials: true,
 })
