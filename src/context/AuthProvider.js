9326d852a47ac80f5b02666dafc323df4ca4aad7
John Marc <jmmalunao@gmail.com>
2022-12-15 22:36:25 +0800

    default

--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -1,11 +1,23 @@
-import { createContext, useState } from "react"
+import { createContext, useState } from 'react'
 
 const AuthContext = createContext({})
 
 export const AuthProvider = ({ children }) => {
-  const [auth, setAuth] = useState({})
+  const [auth, setAuth] = useState({
+    access_token: sessionStorage.getItem('access_token'),
+    id_token: sessionStorage.getItem('id_token'),
+    refresh_token: sessionStorage.getItem('refresh_token'),
+    token_type: sessionStorage.getItem('token_type'),
+    transactionType: sessionStorage.getItem('transactionType'),
+    userType: sessionStorage.getItem('userType'),
+    email: sessionStorage.getItem('email'),
+    email_verified: sessionStorage.getItem('email_verified'),
+    name: sessionStorage.getItem('name'),
+    sub: sessionStorage.getItem('sub'),
+    username: sessionStorage.getItem('username'),
+  })
   const [persist, setPersist] = useState(
-    JSON.parse(localStorage.getItem("persist")) || true
+    JSON.parse(localStorage.getItem('persist')) || true
   )
 
   return (
