36eebf22cbfdae9774be42851ba9a715039d4a1b
John Marc <jmmalunao@gmail.com>
2022-12-16 23:14:38 +0800

    insurance list

--- src/pages/patient/insurance/Insurance.js
+++ src/pages/patient/insurance/Insurance.js
@@ -1,80 +1,136 @@
-import { Link } from "react-router-dom"
+import { useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { AWS_BUCKET } from '../../../constants'
+import useAuth from '../../../hooks/useAuth'
+import useAxiosPrivate from '../../../hooks/useAxiosPrivate'
 
 function Insurance() {
+  const navigate = useNavigate()
+  const { auth } = useAuth()
+  const axiosPrivate = useAxiosPrivate()
+  const [errMsg, setErrMsg] = useState(null)
+  const [list, setList] = useState([])
+
+  useEffect(() => {
+    let isMounted = true
+    const controller = new AbortController()
+
+    async function getList() {
+      await axiosPrivate
+        .post(
+          'getInsurance',
+          { Email: auth.email },
+          {
+            signal: controller.signal,
+          }
+        )
+        .then((res) => {
+          console.log(res)
+          const { Status, Data: data = [], Message } = res.data
+
+          if (Status) {
+            setList(data)
+          } else {
+            throw new Error(Message)
+          }
+        })
+        .catch((err) => {
+          console.error(err)
+          setErrMsg(err.message)
+        })
+    }
+
+    isMounted && getList()
+
+    return () => {
+      isMounted = false
+      controller.abort()
+    }
+  }, [])
+
   return (
-    <div className="page-wrapper">
-      <div className="page-content">
-        <div className="container-fluid">
-          <div className="row figmaFirstBox">
-            <div className="col-sm-12">
+    <div className='page-wrapper'>
+      <div className='page-content'>
+        <div className='container-fluid'>
+          <div className='row figmaFirstBox'>
+            <div className='col-sm-12'>
               <div
-                className="alert alert-warning alert-warning-shadow mb-0 alert-dismissible fade show"
-                role="alert"
+                className='alert alert-warning alert-warning-shadow mb-0 alert-dismissible fade show'
+                role='alert'
               >
                 <button
-                  type="button"
-                  className="close"
-                  data-dismiss="alert"
-                  aria-label="Close"
+                  type='button'
+                  className='close'
+                  data-dismiss='alert'
+                  aria-label='Close'
                 >
-                  <span aria-hidden="true">
-                    <i className="mdi mdi-close"></i>
+                  <span aria-hidden='true'>
+                    <i className='mdi mdi-close'></i>
                   </span>
                 </button>
                 Your insurance document has expired. Please upload a valid
                 document. If you don’t have insurance, please subscribe to
-                <a href="">our monthly plan</a>
+                <a href=''>our monthly plan</a>
               </div>
             </div>
           </div>
 
-          <div className="row">
-            <div className="col-sm-12">
-              <div className="page-title-box">
-                <h4 className="page-title">Insurance</h4>
+          <div className='row'>
+            <div className='col-sm-12'>
+              <div className='page-title-box'>
+                <h4 className='page-title'>Insurance</h4>
               </div>
             </div>
           </div>
 
-          <div className="row">
-            <div className="col-lg-12">
-              <div className="card">
-                <div className="card-body">
-                  <h4 className="header-title mt-0 mb-3">Insurance Document</h4>
+          <div className='row'>
+            <div className='col-lg-12'>
+              <div className='card'>
+                <div className='card-body'>
+                  <h4 className='header-title mt-0 mb-3'>Insurance Document</h4>
 
-                  <div className="file-box-content">
-                    <div className="file-box">
-                      <a href="#" className="download-icon-link">
-                        <i className="dripicons-download file-download-icon"></i>
-                      </a>
-                      <div className="text-center">
-                        <i className="far fa-file-alt text-primary"></i>
-                        <h6 className="text-truncate">
-                          Insurance Michael Steffan
-                        </h6>
-                        <small className="text-muted">
-                          06 March 2022 / 5MB
-                        </small>
+                  <div className='file-box-content'>
+                    {list.map((item) => (
+                      <div className='file-box'>
+                        <Link
+                          to={{
+                            pathname: `${AWS_BUCKET}/insurance/${item.image}`,
+                          }}
+                          target='_blank'
+                          className='download-icon-link'
+                        >
+                          <i className='dripicons-download file-download-icon'></i>
+                        </Link>
+                        <div className='text-center'>
+                          <i className='far fa-file-alt text-primary'></i>
+                          <h6 className='text-truncate'>
+                            {item.provider} {item.type}
+                          </h6>
+                          <small className='text-muted'>
+                            {/* 06 March 2022 / 5MB */}
+                          </small>
+                        </div>
                       </div>
-                    </div>
-                    <div className="file-box">
-                      <a href="#" className="download-icon-link">
-                        <i className="dripicons-download file-download-icon"></i>
+                    ))}
+
+                    {/* <div className='file-box'>
+                      <a href='#' className='download-icon-link'>
+                        <i className='dripicons-download file-download-icon'></i>
                       </a>
-                      <div className="text-center">
-                        <i className="far fa-file-code text-danger"></i>
-                        <h6 className="text-truncate">Insurance.pdf</h6>
-                        <small className="text-muted">
+                      <div className='text-center'>
+                        <i className='far fa-file-code text-danger'></i>
+                        <h6 className='text-truncate'>Insurance.pdf</h6>
+                        <small className='text-muted'>
                           15 March 2022 / 8MB
                         </small>
                       </div>
-                    </div>
+                    </div> */}
                   </div>
 
-                  <Link to="upload">
+                  <Link to='upload'>
                     <button
-                      type="button"
-                      className="btn btn-success btn-round waves-effect waves-light"
+                      type='button'
+                      className='btn btn-success btn-round waves-effect waves-light mt-2'
                     >
                       Upload New Documents
                     </button>
@@ -85,7 +141,7 @@ function Insurance() {
           </div>
         </div>
 
-        <footer className="footer text-center text-sm-left">
+        <footer className='footer text-center text-sm-left'>
           &copy; 2022 NU Health
         </footer>
       </div>
