72e0be6ee8c2fbda72ddf6832cd8fd0409e6ceed
Raseruuu <raseruuu@gmail.com>
2022-12-21 17:06:27 +0800

    add topbar to Patient screen
    Topbar.js in Patient Menu

--- src/pages/patient/layout/PatientDashboard.js
+++ src/pages/patient/layout/PatientDashboard.js
@@ -1,14 +1,31 @@
+import { useMediaQuery } from "@react-hook/media-query"
+import { useEffect, useState } from "react"
 import { Outlet } from "react-router-dom"
 import PatientSideNav from "./PatientSideNav"
+import TopBar from "./TopBar"
 
 function PatientDashboard() {
+  const matches = useMediaQuery("only screen and (max-width: 575.98px)")
+  const [openSideNav, setOpenSideNav] = useState(!matches)
+
+  useEffect(() => {
+    if (matches) {
+      setOpenSideNav(false)
+    }
+  }, [matches])
+
   return (
-    <div
-      className="figma mm-active active"
-      style={{ display: "flex", width: "100vw" }}
-    >
-      <PatientSideNav />
-      <Outlet />
+    <div>
+      <TopBar menuClick={() => setOpenSideNav((prev) => !prev)} />
+      
+      <div
+        className="figma mm-active active"
+        style={{ display: "flex", width: "100vw" }}
+      >
+        {/* <PatientSideNav /> */}
+        <PatientSideNav openSideNav={openSideNav} />
+        <Outlet />
+      </div>
     </div>
   )
 }
