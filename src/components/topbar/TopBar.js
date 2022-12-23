33fb78b9c9396c65d2209727f06524430677dce7
John Marc <jmmalunao@gmail.com>
2022-12-23 12:46:30 +0800

    topbar

--- src/components/topbar/TopBar.js
+++ src/components/topbar/TopBar.js
@@ -3,65 +3,64 @@ import { Link } from 'react-router-dom'
 import SideNavLogo from '../../components/SideNavLogo'
 import { AWS_BUCKET } from '../../constants'
 import useAuth from '../../hooks/useAuth'
-const NotifIconSwitch = (icontype ) => {
-  switch (icontype) {
-    case "order":
-      return 
-    (<div className='avatar-md bg-primary'>
-      <i className='la la-cart-arrow-down text-white'></i>
-    </div>);
-    case "meeting":
-      return 
-    (<div className='avatar-md bg-success'>
-    <i className='la la-group text-white'></i>
-  </div>);
-    default:
-      return (
-    <div className='avatar-md bg-primary'>
-      <i className='la la-cart-arrow-down text-white'></i>
-    </div>
-      )
-  }
+import SAMPLENOTIF from '../../mocks/topbarNotifs'
 
+const NotifIconSwitch = ({ icontype }) => {
+  const iconsGen = {
+    order: {
+      bg: 'bg-primary',
+      icon: 'la-cart-arrow-down',
+    },
+    meeting: {
+      bg: 'bg-success',
+      icon: 'la-group',
+    },
+    success: {
+      bg: 'bg-success',
+      icon: 'la-group',
+    },
+    task: {
+      bg: 'bg-success',
+      icon: 'la-group',
+    },
+  }
+  console.log(icontype)
+  console.log(iconsGen[icontype])
+  return (
+    <div className={`avatar-md ${iconsGen[icontype].bg}`}>
+      <i className={`la ${iconsGen[icontype].icon} text-white`}></i>
+    </div>
+  )
 }
-function NotifLink({ntype,subject,body,timeReceived}){
+function NotifLink({ type, subject, body, timeReceived }) {
   // console.log(notifItem)
   // console.log("body"+notifItem['body  '])
-  const timeNow=20
-  const timeSince=(timeNow-timeReceived) +" minutes ago"
-  return(
+  const timeNow = 20
+  const timeSince = timeNow - timeReceived + ' minutes ago'
+  return (
     <Link to='' className='dropdown-item py-3'>
       <small className='float-right text-muted pl-2'>{timeSince}</small>
       <div className='media'>
-          {/* <NotifIconSwitch icontype={ntype}/> */}
-          
-          <NotifIconSwitch icontype={ntype}/>
+        {/* <NotifIconSwitch icontype={ntype}/> */}
+
+        <NotifIconSwitch icontype={type} />
         <div className='media-body align-self-center ml-2 text-truncate'>
-          <h6 className='my-0 font-weight-normal text-dark'>
-            {subject}
-          </h6>
-          <small className='text-muted mb-0'>
-            {body}
-          </small>
+          <h6 className='my-0 font-weight-normal text-dark'>{subject}</h6>
+          <small className='text-muted mb-0'>{body}</small>
         </div>
       </div>
     </Link>
   )
 }
+
 export function TopBar({ menuClick }) {
-  
-  const [notifs,setNotifs]=useState([
-      {type:"order",subject:"Your order is placed.",body:"Dummy text",timeReceived:4},
-      {type:"meeting",subject:"Meeting With Designers",body:"Lorem Ipsum",timeReceived:10},
-      {type:"task",subject:"Tax Complete",body:"Dolor Sit Amet",timeReceived:10},
-      {type:"order2",subject:"It is a long established fact that a reader...",body:"Dolor Sit Amet",timeReceived:10},
-      {type:"success",subject:"Payment Successful",body:"The Success is in the payment.",timeReceived:10}
-    ])
+  const [notifs, setNotifs] = useState(SAMPLENOTIF)
   //notif badge number
-  const ntfBadgeNum=notifs.length
-  console.log(ntfBadgeNum)
-  console.log(notifs[0].type)
+  const ntfBadgeNum = notifs.length
+  // console.log(ntfBadgeNum)
+  // console.log(notifs[0].type)
   const { auth } = useAuth()
+
   return (
     <div className='dev-top-bar'>
       <div>
@@ -88,28 +87,24 @@ export function TopBar({ menuClick }) {
             aria-expanded='false'
           >
             <i className='ti-bell noti-icon'></i>
-            {ntfBadgeNum>0&&
-            <span className='badge badge-danger badge-pill noti-icon-badge'>
-              {ntfBadgeNum}
-            </span>
-            }
-            
+            {ntfBadgeNum > 0 && (
+              <span className='badge badge-danger badge-pill noti-icon-badge'>
+                {ntfBadgeNum}
+              </span>
+            )}
           </Link>
           <div className='dropdown-menu dropdown-menu-right dropdown-lg pt-0'>
             <h6 className='dropdown-item-text font-15 m-0 py-3 bg-primary text-white d-flex justify-content-between align-items-center'>
               Notifications{' '}
-              <span className='badge badge-light badge-pill'>{ntfBadgeNum}</span>
+              <span className='badge badge-light badge-pill'>
+                {ntfBadgeNum}
+              </span>
             </h6>
             <div className='slimscroll notification-list'>
-              {
-              notifs.map((notif)=>
-                <NotifLink 
-                  type={notif.type}
-                  subject={notif.subject}
-                  body= {notif.body}
-                  timeReceived={notif.timeReceived}/>
-              )
-              }
+              {notifs.map((notif) => {
+                // console.log(notif)
+                return <NotifLink {...notif} />
+              })}
               {/* <NotifLink /> */}
               {/* <Link to='' className='dropdown-item py-3'>
                 <small className='float-right text-muted pl-2'>2 mins. ago</small>
