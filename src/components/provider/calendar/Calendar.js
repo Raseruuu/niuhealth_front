234bcf9e8d9744685ac5a6b73fc7f6a8b02a8019
John Marc <jmmalunao@gmail.com>
2022-12-29 19:42:55 +0800

    visits

--- src/components/provider/calendar/Calendar.js
+++ src/components/provider/calendar/Calendar.js
@@ -1,88 +1,135 @@
-import FullCalendar from "@fullcalendar/react"
-import dayGridPlugin from "@fullcalendar/daygrid"
-import interactionPlugin from "@fullcalendar/interaction"
-import timeGridPlugin from "@fullcalendar/timegrid"
-
-let eventGuid = 0
-let todayStr = new Date().toISOString().replace(/T.*$/, "") // YYYY-MM-DD of today
-
-export const INITIAL_EVENTS = [
-  {
-    id: createEventId(),
-    title: "All-day event",
-    start: todayStr,
-  },
-  {
-    id: createEventId(),
-    title: "Timed event",
-    start: todayStr + "T12:00:00",
-  },
-]
-
-export function createEventId() {
-  return String(eventGuid++)
-}
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import { useEffect, useState } from 'react'
+import useAxiosPrivate from '../../../hooks/useAxiosPrivate'
+import useAuth from '../../../hooks/useAuth'
+import moment from 'moment'
+import Swal from 'sweetalert2'
 
 export default function Calendar() {
+  const { auth } = useAuth()
+  const [slots, setSlots] = useState([])
+  const axiosPrivate = useAxiosPrivate()
+  const [errMsg, setErrMsg] = useState(null)
+
   const handleDateSelect = (selectInfo) => {
     console.log(selectInfo)
 
-    let title = prompt("Please enter a new title for your event")
-    let calendarApi = selectInfo.view.calendar
+    // let title = prompt('Please enter a new title for your event')
+    // let calendarApi = selectInfo.view.calendar
 
-    calendarApi.unselect() // clear date selection
+    // calendarApi.unselect() // clear date selection
 
-    if (title) {
-      calendarApi.addEvent({
-        id: createEventId(),
-        title,
-        start: selectInfo.startStr,
-        end: selectInfo.endStr,
-        allDay: selectInfo.allDay,
-      })
-    }
+    // if (title) {
+    //   calendarApi.addEvent({
+    //     id: createEventId(),
+    //     title,
+    //     start: selectInfo.startStr,
+    //     end: selectInfo.endStr,
+    //     allDay: selectInfo.allDay,
+    //   })
+    // }
   }
 
   const handleEventClick = (clickInfo) => {
     console.log(clickInfo)
-    if (
-      window.confirm(
-        `Are you sure you want to delete the event '${clickInfo.event.title}'`
-      )
-    ) {
-      clickInfo.event.remove()
-    }
+
+    Swal.fire(clickInfo.event.title)
   }
 
   const handleEvents = (events) => {
     console.log(events)
   }
 
+  const INITIAL_EVENTS = (appointments = []) => {
+    const schedArray = appointments.map((item) => {
+      console.log(item)
+
+      const currentD = moment(item.trans_date_time).format('YYYY-MM-DD')
+
+      let timeStr = Number(item.trans_start)
+      if (timeStr < 10) {
+        timeStr = '0' + timeStr
+      }
+
+      const startStr = `${currentD}T${timeStr}:00:00`
+
+      return {
+        id: item.appointment_id,
+        title: item.service_id,
+        start: startStr,
+        backgroundColor: '#1eca7b',
+        borderColor: 'transparent',
+      }
+    })
+
+    setSlots(schedArray)
+  }
+
+  useEffect(() => {
+    let isMounted = true
+    const controller = new AbortController()
+
+    async function getSched() {
+      await axiosPrivate
+        .post(
+          'getProviderAppointments',
+          { Email: auth.email },
+          {
+            signal: controller.signal,
+          }
+        )
+        .then((res) => {
+          console.log(res)
+          const { Status, Data: data = [], Message } = res.data
+
+          if (Status) {
+            isMounted && INITIAL_EVENTS(data)
+          } else {
+            throw new Error(Message)
+          }
+        })
+        .catch((err) => {
+          console.error(err)
+          setErrMsg(err.message)
+        })
+    }
+
+    getSched()
+
+    return () => {
+      isMounted = false
+      controller.abort()
+    }
+  }, [])
+
   return (
     <FullCalendar
       plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
       headerToolbar={{
-        left: "prev,next today",
-        center: "title",
-        right: "dayGridMonth,timeGridWeek,timeGridDay",
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay',
       }}
       initialView='dayGridMonth'
-      initialEvents={INITIAL_EVENTS}
-      editable={true}
+      events={slots}
+      // editable={true}
       selectable={true}
       selectMirror={true}
       dayMaxEvents={true}
       select={handleDateSelect}
       eventClick={handleEventClick}
-      eventsSet={handleEvents}
+      // eventsSet={handleEvents}
       eventAdd={function ({ event }) {
-        console.log("eventAdd", event)
+        console.log('eventAdd', event)
       }}
       eventChange={function ({ event }) {
-        console.log("eventChange", event)
+        console.log('eventChange', event)
       }}
       eventRemove={function (event) {
-        console.log("eventRemove", event)
+        console.log('eventRemove', event)
       }}
     />
   )
--- src/pages/provider/VisitRequest.js
+++ src/pages/provider/VisitRequest.js
@@ -1,64 +1,108 @@
-import { useEffect, useState } from "react"
-
-function PatientRequest({patientReqData}){
-  const req_id=patientReqData.id
-  return patientReqData.map((item, index)=>(
-      <tr>
-      <td>{item.id}</td>
-      <td>
-        <div className='media'>
-          <a href='../pages/visit-request-profile.html'>
-            <img
-              src='../assets/images/users/user-1.png'
-              alt=''
-              className='thumb-sm rounded-circle mr-2'
-            />
-          </a>
-          <div className='media-body align-self-center text-truncate'>
-            <h6 className='mt-0 mb-1 text-dark'> 
-              <a href='../pages/visit-request-profile.html'>
-                {item.name}
-              </a>{" "}
-              <span className={'badge badge-md '+(item.insurType ? 'badge-soft-success':'badge-soft-danger')}>
-                {(item.insurType ? 'Insured' : 'Not Insured')}
-              </span>
-            </h6>
-            <p className='text-muted mb-0'>
-              Virtual visit on {item.schedule}
-            </p>
-          </div>
-        </div>
-      </td>
-      <td>{item.requestDate}</td>
-      <td>
-        {" "}
-        <button
-          type='button'
-          className='btn btn-outline-success waves-effect waves-light'
-          onclick="approveVisit('Donald');"
-        >
-          Approve
-        </button>{" "}
-        <button
-          type='button'
-          className='btn btn-outline-danger waves-effect waves-light'
-          onclick="cancelVisit('Donald');"
-        >
-          Cancel
-        </button>
-      </td>
-    </tr>
-  ))
-}
+import moment from 'moment'
+import { useEffect, useMemo, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import useAuth from '../../hooks/useAuth'
+import useAxiosPrivate from '../../hooks/useAxiosPrivate'
+import Swal from 'sweetalert2'
 
 function VisitRequest() {
-  const [reqList, setReqList] = 
-    useState([
-      {id:546546, name: "Donald Gardner", insurType: 0,schedule:"15 August",requestDate:"03 Aug 2023 1:33"},
-      {id:504152, name: "Nicholas Smith", insurType: 1,schedule:"21 September",requestDate:"03 Dec 2023 5:00"},
-      {id:240879, name: "Paula Anderson", insurType: 1,schedule:"5 December",requestDate:"02 Sep 2023 12:33"}
-    ])
-  
+  const actionX = useMemo(() => ({ approve: 'approve', cancel: 'cancel' }), [])
+
+  const navigate = useNavigate()
+  const { auth, setAuth } = useAuth()
+  const axiosPrivate = useAxiosPrivate()
+  const [errMsg, setErrMsg] = useState(null)
+  const [list, setList] = useState([])
+  const [refreshList, setRefreshList] = useState(false)
+
+  function handleActionClick(action, selectedItem) {
+    console.log(action)
+    console.log(selectedItem)
+
+    Swal.fire({
+      title:
+        action === actionX.approve
+          ? 'Approve Appointment'
+          : 'Cancel Appointment',
+      html: `Are you sure you want to ${action} appointment?
+      <span class="text-muted">Patient: ${selectedItem.email}</span><br />
+      <span class="text-muted">Date and Time: ${dateGenerator(
+        selectedItem.trans_date_time,
+        selectedItem.trans_start
+      )}
+        </span>`,
+      icon: action === actionX.approve ? 'question' : 'warning',
+      confirmButtonText: 'Yes',
+      showCancelButton: true,
+      focusConfirm: false,
+      focusCancel: true,
+    }).then(async ({ isConfirmed }) => {
+      if (isConfirmed) {
+        await axiosPrivate
+          .post(
+            action === actionX.approve
+              ? 'approveVisitRequest'
+              : 'cancelVisitRequest',
+            {
+              Email: auth.email,
+              AppointmentID: selectedItem.visit_id,
+            }
+          )
+          .then((res) => {
+            if (res.data?.Status && action === actionX.approve) {
+              Swal.fire('Appointment successfully approved.')
+            } else if (res.data?.Status && action === actionX.cancel) {
+              Swal.fire('Appointment successfully cancelled.')
+            } else {
+              Swal.fire(res.data?.Message)
+            }
+          })
+          .catch((err) => {
+            console.error(err)
+            Swal.fire('Action failed.')
+          })
+        setRefreshList((prev) => !prev)
+      }
+    })
+  }
+
+  useEffect(() => {
+    let isMounted = true
+    const controller = new AbortController()
+
+    async function getList() {
+      await axiosPrivate
+        .post(
+          'getAppointmentRequests',
+          { Email: auth.email },
+          {
+            signal: controller.signal,
+          }
+        )
+        .then((res) => {
+          console.log(res)
+          const { Status, Data: data = [], Message } = res.data
+
+          if (Status) {
+            isMounted && setList(data)
+          } else {
+            throw new Error(Message)
+          }
+        })
+        .catch((err) => {
+          console.error(err)
+          setErrMsg(err.message)
+        })
+    }
+
+    getList()
+
+    return () => {
+      isMounted = false
+      controller.abort()
+    }
+  }, [refreshList])
+
   return (
     <div className='container-fluid'>
       <div className='row'>
@@ -84,99 +128,78 @@ function VisitRequest() {
                       <th className='border-top-0'>Action</th>
                     </tr>
                   </thead>
-                
+
                   <tbody>
-                  
-                    <PatientRequest patientReqData = {reqList}/>
-                    
-                   {/* <tr>
-                      <td>504152</td>
-                      <td>
-                        <div className='media'>
-                          <a href='../pages/visit-request-profile.html'>
-                            <div className='avatar-box thumb-sm align-self-center mr-2'>
-                              <span className='avatar-title bg-soft-pink rounded-circle'>
-                                NS
-                              </span>
+                    {list.map((item, index) => (
+                      <tr key={item?.appointment_id || index}>
+                        <td>{item.visit_id}</td>
+                        <td>
+                          <div className='media'>
+                            <a href='../pages/visit-request-profile.html'>
+                              <img
+                                src='../assets/images/users/user-1.png'
+                                alt=''
+                                className='thumb-sm rounded-circle mr-2'
+                              />
+                            </a>
+                            <div className='media-body align-self-center text-truncate'>
+                              <h6 className='mt-0 mb-1 text-dark'>
+                                <a href='../pages/visit-request-profile.html'>
+                                  {item.name}
+                                </a>{' '}
+                                <span
+                                  className={
+                                    'badge badge-md ' +
+                                    (item.insurType
+                                      ? 'badge-soft-success'
+                                      : 'badge-soft-danger')
+                                  }
+                                >
+                                  {item.insurType ? 'Insured' : 'Not Insured'}
+                                </span>
+                              </h6>
+                              <p className='text-muted mb-0'>
+                                Virtual visit on{' '}
+                                {dateGenerator(
+                                  item.trans_date_time,
+                                  item.trans_start
+                                )}
+                              </p>
                             </div>
-                          </a>
-                          <div className='media-body align-self-center text-truncate'>
-                            <h6 className='mt-0 mb-1 text-dark'>
-                              <a href='../pages/visit-request-profile.html'>
-                                Nicholas Smith
-                              </a>{" "}
-                              <span className='badge badge-md badge-soft-success'>
-                                Insured
-                              </span>
-                            </h6>
-                            <p className='text-muted mb-0'>
-                              Virtual visit on 15 August
-                            </p>
                           </div>
-                        </div>
-                      </td>
-                      <td>03 Aug 2022 12:33</td>
-                      <td>
-                        <button
-                          type='button'
-                          className='btn btn-outline-success waves-effect waves-light'
-                          onclick="approveVisit('Nicholas');"
-                        >
-                          Approve
-                        </button>{" "}
-                        <button
-                          type='button'
-                          className='btn btn-outline-danger waves-effect waves-light'
-                          onclick="cancelVisit('Nicholas');"
-                        >
-                          Cancel
-                        </button>
-                      </td>
-                    </tr>
-                     <tr>
-                      <td>240879</td>
-                      <td>
-                        <div className='media'>
-                          <a href='../pages/visit-request-profile.html'>
-                            <img
-                              src='../assets/images/users/user-4.jpg'
-                              alt=''
-                              className='thumb-sm rounded-circle mr-2'
-                            />
-                          </a>
-                          <div className='media-body align-self-center text-truncate'>
-                            <h6 className='mt-0 mb-1 text-dark'>
-                              <a href='../pages/visit-request-profile.html'>
-                                Paula Anderson
-                              </a>{" "}
-                              <span className='badge badge-md badge-soft-success'>
-                                Insured
-                              </span>
-                            </h6>
-                            <p className='text-muted mb-0'>
-                              Virtual visit on 15 August
-                            </p>
-                          </div>
-                        </div>
-                      </td>
-                      <td>03 Aug 2022 12:33</td>
-                      <td>
-                        <button
-                          type='button'
-                          className='btn btn-outline-success waves-effect waves-light'
-                          onclick="approveVisit('Paula');"
-                        >
-                          Approve
-                        </button>{" "}
-                        <button
-                          type='button'
-                          className='btn btn-outline-danger waves-effect waves-light'
-                          onclick="cancelVisit('Paula');"
-                        >
-                          Cancel
-                        </button>
-                      </td>
-                    </tr> */}
+                        </td>
+                        <td>
+                          {dateGenerator(
+                            item.trans_date_time,
+                            item.trans_start
+                          )}
+                        </td>
+                        <td>
+                          <button
+                            type='button'
+                            className='btn btn-outline-success waves-effect waves-light'
+                            onClick={handleActionClick.bind(
+                              this,
+                              actionX.approve,
+                              item
+                            )}
+                          >
+                            Approve
+                          </button>{' '}
+                          <button
+                            type='button'
+                            className='btn btn-outline-danger waves-effect waves-light'
+                            onClick={handleActionClick.bind(
+                              this,
+                              actionX.cancel,
+                              item
+                            )}
+                          >
+                            Cancel
+                          </button>
+                        </td>
+                      </tr>
+                    ))}
                   </tbody>
                 </table>
               </div>
@@ -186,6 +209,10 @@ function VisitRequest() {
       </div>
     </div>
   )
+}
+
+function dateGenerator(date, time) {
+  return moment(date).add(time, 'hours').format('DD MMM YYYY h:mm a')
 }
 
 export default VisitRequest
