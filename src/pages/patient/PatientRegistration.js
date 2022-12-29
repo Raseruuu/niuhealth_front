7a5e908558f860788216ca7264625e14c02a68c7
Raseruuu <raseruuu@gmail.com>
2022-12-29 19:30:27 +0800

    input fields

--- src/pages/patient/PatientRegistration.js
+++ src/pages/patient/PatientRegistration.js
@@ -1,6 +1,17 @@
 import { useNavigate } from 'react-router-dom'
 import Footer from '../../components/Footer'
-
+function InputField({labeltext}){
+    return(
+        <div class="form-group row">
+            <div class="col-md-12">
+                <label for="example-text-input" class="col-form-label text-right">
+                    {labeltext}
+                </label>
+                <input class="form-control" type="text" value="" id="example-text-input"/>
+            </div>
+        </div>
+    )
+}
 function PatientRegistration() {
 
   return (
@@ -10,69 +21,69 @@ function PatientRegistration() {
     {/* <!-- end page title end breadcrumb --> */}
         <div class="row "> 
         <div class="col-lg-6">
-          <div class="card">
-              <div class="card-body">
-              <h3>Thanks for registering  to NIU Health Social.</h3>
-              <h5>We need a few more items to get started</h5>
-              <div class="form-group row">
-                <div class="col-md-12">
-          <label for="example-text-input" class="col-form-label text-right">Phone Number</label>
-          <input class="form-control" type="text" value="" id="example-text-input"/>
-        </div>
-                            </div>
-  
-  
-  <div class="form-group row">
-                                <div class="col-md-12">
-        <label for="example-text-input" class="col-form-label text-right">Address</label></div>
-                                <div class="col-md-12">
-                                    <input class="form-control" type="text" value="" id="example-text-input" />
-                                </div>
-                            </div>
-      <div class="form-group row">
-                                <div class="col-md-6">
-          <label for="example-text-input" class="col-form-label text-right">Country</label>
-          <select class="form-control">
-                                                        <option>Choose Country...</option>
-                                                        <option>USA</option>
-                                                        <option>South Korea</option>
-                                                        <option>Canada</option>
-                                                        <option>Thailand</option>
-                                                    </select>
-        </div>
-                                <div class="col-md-6">
-          <label for="example-text-input" class="col-form-label text-right">City</label>
-                                    <select class="form-control">
-                                                        <option>Choose City...</option>
-                                                    </select>
-                                </div>
-                            </div>
-  
-    <div class="form-group row">
-                                <div class="col-md-12">
-          <label for="example-text-input" class="col-form-label text-right">
-Subscription may cost more for patients not residing in Hawaii. <a href="">Know more</a></label>
-        </div>
-    </div>
+            <div class="card">
+                <div class="card-body">
+                <h3>Thanks for registering  to NIU Health Social.</h3>
+                <h5>We need a few more items to get started</h5>
+                {/* <div class="form-group row">
+                    <div class="col-md-12">
+                        <label for="example-text-input" class="col-form-label text-right">
+                            Phone Number
+                        </label>
+                        <input class="form-control" type="text" value="" id="example-text-input"/>
+                    </div>
+                </div> */}
+                <InputField labeltext = {"Email"}/>
+                <InputField labeltext = {"First Name"}/>
+                <InputField labeltext = {"Last Name"}/>
+                <InputField labeltext = {"Middle Name"}/>
+                <InputField labeltext = {"Contact Info"}/>
+                <InputField labeltext = {"Address"}/>
+                {/* <div class="form-group row">
+                    <div class="col-md-12">
+                        <label for="example-text-input" class="col-form-label text-right">Address</label>
+                    </div>
+                    <div class="col-md-12">
+                        <input class="form-control" type="text" value="" id="example-text-input" />
+                    </div>
+                </div> */}
+                <div class="form-group row">
+                    <div class="col-md-6">
+                        <label for="example-text-input" class="col-form-label text-right">Country</label>
+                        <select class="form-control">
+                            <option>Choose Country...</option>
+                            <option>USA</option>
+                            <option>South Korea</option>
+                            <option>Canada</option>
+                            <option>Thailand</option>
+                        </select>
+                    </div>
+                    <div class="col-md-6">
+                        <label for="example-text-input" class="col-form-label text-right">City</label>
+                            <select class="form-control">
+                                <option>Choose City...</option>
+                            </select>
+                        </div>
+                    </div>
+            
+                    <div class="form-group row">
+                        <div class="col-md-12">
+                            <label for="example-text-input" class="col-form-label text-right">
+                            Subscription may cost more for patients not residing in Hawaii. <a href="">Know more</a></label>
+                        </div>
+                    </div>
       
       
-  <div class="form-group row">
-        <div class="col-md-6">
-            <label for="example-date-input" class="">Date of Birth</label>
-            <input class="form-control" type="date" value="2011-08-19" id="example-date-input" />
-        </div>
-    </div>		
-      
-  
-  
-  
-  
-  <div class="wizard_btn" style={{marginBottom: "50px"}}> 
-    
-      <a href="welcome-step2.html"><button type="button" class="btn btn-success btn-round waves-effect waves-light figmaBigButton float-left postRegBtn">Continue</button>
-      </a>
-  </div>
-
+                    <div class="form-group row">
+                        <div class="col-md-6">
+                            <label for="example-date-input" class="">Date of Birth</label>
+                            <input class="form-control" type="date" value="2011-08-19" id="example-date-input" />
+                        </div>
+                    </div>		
+                    <div class="wizard_btn" style={{marginBottom: "50px"}}> 
+                        <a href="welcome-step2.html"><button type="button" class="btn btn-success btn-round waves-effect waves-light figmaBigButton float-left postRegBtn">Continue</button>
+                        </a>
+                    </div>
                 </div>
             </div>                           
         </div>
@@ -80,37 +91,28 @@ Subscription may cost more for patients not residing in Hawaii. <a href="">Know 
 
 
 <div class="col-lg-6">
-            <div class="card">
-                <div class="card-body">
-
-    
-  
-                    <div class="media">
-                                      
-                        <div class="media-body align-self-center">
-                            <h3 class="mt-0 mb-1">How can NIU help you?</h3>
-                           
-                        </div>
+    <div class="card">
+        <div class="card-body">
+            <div class="media">       
+                <div class="media-body align-self-center">
+                    <h3 class="mt-0 mb-1">How can NIU help you?</h3>
+                </div>
                    
 
-  <div class="pricingTable1 text-center">
-                        
-                       
-                        <ul class="list-unstyled pricing-content-2 text-left py-3 border-0 mb-0">
-                            <li>Unlimited access to Virtual and In-Person Visits</li>
-                            <li>Unlimited access to NIU Health In-Person Visits</li>
-                            <li>With health insurance, you get a free subscription and unlimited visits</li>
-      <li>Without health insurance, you pay just $9.99 per month for unlimited visits</li>
-                        </ul>
+                        <div class="pricingTable1 text-center">
+                            <ul class="list-unstyled pricing-content-2 text-left py-3 border-0 mb-0">
+                                <li>Unlimited access to Virtual and In-Person Visits</li>
+                                <li>Unlimited access to NIU Health In-Person Visits</li>
+                                <li>With health insurance, you get a free subscription and unlimited visits</li>
+                                <li>Without health insurance, you pay just $9.99 per month for unlimited visits</li>
+                            </ul>
+                        </div>
                     </div>
-  <div>
-    
-  </div>
-                          </div>
-                      </div>                            
-                  </div>
-          </div></div>
+                </div>                            
+            </div>
+        </div>
     </div>
+</div>
         <Footer />
       </div>
     </div>
