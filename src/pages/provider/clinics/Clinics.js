8473f31fc82e686d6fb6962e3d69a7d3f13f6549
John Marc <jmmalunao@gmail.com>
2023-01-03 16:47:28 +0800

    card type

--- src/pages/provider/clinics/Clinics.js
+++ src/pages/provider/clinics/Clinics.js
@@ -70,54 +70,25 @@ function Clinics() {
       </div>
 
       <div className='row'>
-        <div className='col-lg-12'>
-          <div className='card'>
-            <div className='card-body'>
-              <div className='table-responsive'>
-                <table className='table'>
-                  <thead className='thead-light'>
-                    <tr>
-                      <th>Clinic</th>
-                      <th>Address</th>
-                      <th>Services</th>
-                      <th>Working Hours</th>
-                      <th>Action</th>
-                    </tr>
-                  </thead>
-
-                  <tbody>
-                    {list.map((item, index) => {
-                      return (
-                        <tr key={item?.recno || index}>
-                          <td>
-                            <img
-                              src={`${AWS_BUCKET}/assets/images/users/user-10.jpg`}
-                              alt='user-10'
-                              className='thumb-sm rounded-circle mr-2'
-                            />
-                            {item.name}
-                          </td>
-                          <td>{item.address}</td>
-                          <td>{item.services}</td>
-                          <td>{item.working_hours}</td>
-
-                          <td>
-                            <Link to='#' className='mr-2'>
-                              <i className='fas fa-edit text-info font-16'></i>
-                            </Link>
-                            <Link to='#'>
-                              <i className='fas fa-trash-alt text-danger font-16'></i>
-                            </Link>
-                          </td>
-                        </tr>
-                      )
-                    })}
-                  </tbody>
-                </table>
+        {list.map((item) => (
+          <div className='col-sm-12 col-md-4'>
+            <div class='card flex-sm-col flex-md-row overflow-hidden'>
+              <img
+                class='card-img-top'
+                src={`${AWS_BUCKET}/assets/images/users/user-10.jpg`}
+                alt=''
+              />
+              <div class='card-body'>
+                <h5 class='card-title'>{item.name || 'Clinic Name Sample'}</h5>
+                <p class='card-text mb-0'>{item.address}</p>
+                <p className='text-muted mb-0'>
+                  {item.services || 'Neurologist / Sleep Doctor / Surgeon'}
+                </p>
+                <p className='mb-0'>{item.working_hours || `Mon 8am - 5pm`}</p>
               </div>
             </div>
           </div>
-        </div>
+        ))}
       </div>
     </div>
   )
