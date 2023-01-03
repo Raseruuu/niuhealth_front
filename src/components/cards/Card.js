1bd6ab927f7d4d27245c755289551024dbd33af6
Raseruuu <raseruuu@gmail.com>
2023-01-03 17:54:42 +0800

    Clinic Card component
    created components/cards

---
+++ src/components/cards/Card.js
@@ -0,0 +1,17 @@
+
+export default function CardItem({image,children}){
+    return(
+        <div className='col-sm-12 col-md-4'>
+            <div class='card flex-sm-col flex-md-row overflow-hidden'>
+                <img
+                class='card-img-top'
+                src={image}
+                alt=''
+                />
+                <div class='card-body'>
+                    {children}
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/pages/provider/clinics/Clinics.js
+++ src/pages/provider/clinics/Clinics.js
@@ -3,8 +3,9 @@ import { Link, useNavigate } from 'react-router-dom'
 import { AWS_BUCKET } from '../../../constants'
 import useAuth from '../../../hooks/useAuth'
 import useAxiosPrivate from '../../../hooks/useAxiosPrivate'
-import TableCard, { TableTextLink , TableTitle } from "../../../components/table/Tables"
 
+import {TableTitle} from "../../../components/table/Tables"
+import CardItem from "../../../components/cards/Card"
 function Clinics() {
   const navigate = useNavigate()
   const { auth, setAuth } = useAuth()
@@ -64,73 +65,19 @@ function Clinics() {
           </ol>
         </div>
       </TableTitle>
-      <TableCard headers={["Clinic","Address","Services","Working Hours","Action"]}>
-        {list.map((item, index) => {
-          return (
-            <tr key={item?.recno || index}>
-              <td>
-                <img
-                  src={`${AWS_BUCKET}/assets/images/users/user-10.jpg`}
-                  alt='user-10'
-                  className='thumb-sm rounded-circle mr-2'
-                />
-                {item.clinic_name}
-              </td>
-              <td>{item.address}</td>
-              <td>{item.services}</td>
-              <td>{item.working_hours}</td>
-
-              <td>
-                <Link to='#' className='mr-2'>
-                  <i className='fas fa-edit text-info font-16'></i>
-                </Link>
-                <Link to='#'>
-                  <i className='fas fa-trash-alt text-danger font-16'></i>
-                </Link>
-              </td>
-            </tr>
-          )
-        })}
-      </TableCard>
       
-      <div className='row'>
-        <div className='col-sm-12'>
-          <div className='page-title-box'>
-            {/* <div className='float-right'>
-              <ol className='breadcrumb'>
-                <button
-                  type='button'
-                  className='btn btn-success waves-effect waves-light'
-                  onClick={() => navigate('create')}
-                >
-                  New Clinic Schedule
-                </button>
-              </ol>
-            </div> */}
-            <h4 className='page-title'>Clinics</h4>
-          </div>
-        </div>
-      </div>
+      
 
       <div className='row'>
         {list.map((item) => (
-          <div className='col-sm-12 col-md-4'>
-            <div class='card flex-sm-col flex-md-row overflow-hidden'>
-              <img
-                class='card-img-top'
-                src={`${AWS_BUCKET}/assets/images/users/user-10.jpg`}
-                alt=''
-              />
-              <div class='card-body'>
-                <h5 class='card-title'>{item.name || 'Clinic Name Sample'}</h5>
-                <p class='card-text mb-0'>{item.address}</p>
-                <p className='text-muted mb-0'>
-                  {item.services || 'Neurologist / Sleep Doctor / Surgeon'}
-                </p>
-                <p className='mb-0'>{item.working_hours || `Mon 8am - 5pm`}</p>
-              </div>
-            </div>
-          </div>
+          <CardItem image={`${AWS_BUCKET}/assets/images/users/user-10.jpg`}>
+            <h5 class='card-title'>{item.clinic_name || 'Clinic Name Sample'}</h5>
+                    <p class='card-text mb-0'>{item.address}</p>
+                    <p className='text-muted mb-0'>
+                        {item.services || 'Neurologist / Sleep Doctor / Surgeon'}
+                    </p>
+                    <p className='mb-0'>{item.working_hours || `Mon 8am - 5pm`}</p>
+          </CardItem>
         ))}
       </div>
     </div>
