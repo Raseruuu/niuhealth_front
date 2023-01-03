5d3b9d3f205fa3c14e0ad51b217e965afd9c752e
John Marc <jmmalunao@gmail.com>
2023-01-03 10:43:18 +0800

    removed comma

--- src/components/provider/WelcomeCard.js
+++ src/components/provider/WelcomeCard.js
@@ -3,12 +3,15 @@ import useAuth from '../../hooks/useAuth'
 
 function WelcomeCard() {
   const { auth } = useAuth()
-  const [greeting,setGreeting] = useState("Aloha, ")
+  const [greeting, setGreeting] = useState('Aloha ')
   return (
     <div className='card'>
       <div className='card-body'>
         <div className='jumbotron mb-0 bg-light'>
-          <h1 className='display-4'>{greeting}{auth?.name ?? null}!</h1>
+          <h1 className='display-4'>
+            {greeting}
+            {auth?.name ?? null}!
+          </h1>
           <p className='lead'>Have a nice day at work!</p>
           {/* <hr className="my-4" /> */}
         </div>
