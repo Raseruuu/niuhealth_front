eb536825ea162a9a7654efec112c8fef95f409b1
John Marc M <jmmalunao@gmail.com>
2023-01-11 15:42:21 +0800

    tell us why

--- src/pages/virtualvisit/TellUsWhy.js
+++ src/pages/virtualvisit/TellUsWhy.js
@@ -7,19 +7,43 @@ export default function TellUsWhy() {
   const { auth } = useAuth()
   const axiosPrivate = useAxiosPrivate()
   const navigate = useNavigate()
-  const [symptomTextarea, setSymptomTextArea] = useState('')
   const [symptom, setSymptom] = useState('')
+  const [selectedSymptom, setSelectedSymptom] = useState([])
   const [isSubmitting, setIsSubmitting] = useState(false)
+
+  function handleButtonClick(selected) {
+    const currentSelected = new Set(selectedSymptom)
+
+    if (currentSelected.has(selected)) return
+
+    currentSelected.add(selected)
+    setSelectedSymptom((prev) => [...prev, selected])
+
+    if (symptom.trim().length <= 0) {
+      setSymptom(selected)
+    } else {
+      setSymptom((prev) => `${prev}, ${selected}`)
+    }
+  }
+
+  function handleTextarea(e) {
+    const val = e.target.value
+    setSymptom(val)
+
+    if (val.length <= 0) {
+      setSelectedSymptom([])
+    }
+  }
+
   const handleSubmit = async () => {
     const controller = new AbortController()
 
-    setIsSubmitting(true)
     try {
-      console.log(symptomTextarea || symptom)
+      setIsSubmitting(true)
       await axiosPrivate
         .post(
           'patientSaveSymptoms',
-          { Email: auth.email, Symptoms: symptomTextarea || symptom },
+          { Email: auth.email, Symptoms: symptom },
           {
             signal: controller.signal,
           }
@@ -45,16 +69,17 @@ export default function TellUsWhy() {
       console.error(error)
     }
   }
+
   return (
-    <div className='page-wrapper mt-0'>
+    <div className="page-wrapper mt-0">
       {/* <!-- Page Content--> */}
-      <div className='page-content'>
-        <div className='container-fluid'>
+      <div className="page-content">
+        <div className="container-fluid">
           {/* <!-- Page-Title --> */}
-          <div className='row'>
-            <div className='col-sm-12'>
-              <div className='page-title-box'>
-                <h4 className='page-title'></h4>
+          <div className="row">
+            <div className="col-sm-12">
+              <div className="page-title-box">
+                <h4 className="page-title"></h4>
               </div>
               {/* <!--end page-title-box--> */}
             </div>
@@ -62,63 +87,70 @@ export default function TellUsWhy() {
           </div>
           {/* <!-- end page title end breadcrumb --> */}
 
-          <div className='row '>
-            <div className='col-lg-6'>
-              <div className='card'>
-                <div className='card-body'>
+          <div className="row ">
+            <div className="col-lg-6">
+              <div className="card">
+                <div className="card-body">
                   <h3>Tell us why you’re here</h3>
 
-                  {/* <Textarea/> */}
-                  <select
-                    id='symptoms'
-                    className='form-control'
-                    onChange={(choice) => {
-                      setSymptom(choice.target.value)
-                      console.log(choice.target.value)
-                    }}
+                  <textarea
+                    style={{ margin: '30px 0 0 0' }}
+                    className="form-control"
+                    rows="5"
+                    id="message"
+                    placeholder="Add a reason for your virtual visit"
+                    value={symptom}
+                    onChange={handleTextarea}
+                    maxLength="500"
+                  ></textarea>
+                  <div
+                    className="d-flex  flex-row justify-content-start align-items-center overflow-auto"
+                    style={{ height: '50px' }}
                   >
-                    <option value=''></option>
-                    <option value='Headache'>Headache</option>
-                    <option value='Stomachache'>Stomachache</option>
-                    <option value='Cough'>Cough</option>
-                    <option value='Colds'>Colds</option>
-                    <option value='Skin Infection'>Skin Infection</option>
-                    <option value='Other'>Other</option>
-                  </select>
-                  {symptom === 'Other' ? (
-                    <textarea
-                      style={{ margin: '30px 0 20px 0' }}
-                      className='form-control'
-                      rows='5'
-                      id='message'
-                      placeholder='Add a reason for your virtual visit'
-                      value={symptomTextarea}
-                      onChange={(e) => {
-                        setSymptomTextArea(e.target.value)
-                      }}
-                    ></textarea>
-                  ) : null}
+                    {[
+                      'Headache',
+                      'Stomachache',
+                      'Cough',
+                      'Skin Infection',
+                      'Colds',
+                    ].map((e) => (
+                      <button
+                        key={e}
+                        type="button"
+                        className="btn btn-light btn-sm mr-1 text-nowrap"
+                        style={{
+                          boxShadow: 'unset',
+                          borderRadius: '15px',
+                        }}
+                        onClick={handleButtonClick.bind(this, e)}
+                      >
+                        {e}
+                      </button>
+                    ))}
+                  </div>
+
                   {/* <p className='text-muted mb-3'>Upload your files here</p> */}
                   {/* <input type='file' id='input-file-now' className='dropify' /> */}
-                  <div className='wizard_btn' style={{ marginBottom: '50px' }}>
+                  <div
+                    className="wizard_btn  "
+                    style={{ marginBottom: '50px' }}
+                  >
                     {/* <Link to='waitingroom'> */}
                     <button
-                      type='button'
-                      className='btn btn-success btn-round waves-effect waves-light figmaBigButton float-left'
+                      type="button"
+                      className="mb-1 mb-md-0 btn btn-success btn-round waves-effect waves-light figmaBigButton float-left"
                       onClick={handleSubmit}
-                      disabled={
-                        isSubmitting ||
-                        !symptom ||
-                        (symptom === 'Other' && !symptomTextarea)
-                      }
+                      disabled={isSubmitting || !symptom}
                     >
-                      Start Your First Virtual Visit
+                      {isSubmitting
+                        ? 'Please wait...'
+                        : 'Start Your Virtual Visit'}
                     </button>
                     {/* </Link> */}
-                    <Link to='..'>
+                    <Link to="..">
                       <button
-                        type='button'
-                        className='btn btn-danger btn-round waves-effect waves-light figmaBigButton float-bottom'
+                        type="button"
+                        className="btn btn-danger btn-round waves-effect waves-light figmaBigButton float-bottom"
                       >
                         Cancel
                       </button>
@@ -134,18 +166,18 @@ export default function TellUsWhy() {
               </div>
               {/* <!--end card-->                                    */}
             </div>
-            <div className='col-lg-6'>
-              <div className='card'>
-                <div className='card-body'>
+            <div className="col-lg-6">
+              <div className="card">
+                <div className="card-body">
                   <img
-                    src='../assets/images/getting-consultation-of-doctor.jpg'
-                    className='steps_img'
-                    alt=''
+                    src="../assets/images/getting-consultation-of-doctor.jpg"
+                    className="steps_img"
+                    alt=""
                   />
 
-                  <div className='media'>
-                    <div className='media-body align-self-center'>
-                      <h3 className='mt-0 mb-1'>
+                  <div className="media">
+                    <div className="media-body align-self-center">
+                      <h3 className="mt-0 mb-1">
                         How to get the most out of your visit
                       </h3>
                     </div>
@@ -153,8 +185,8 @@ export default function TellUsWhy() {
                   </div>
                   {/* <!--end media--> */}
 
-                  <div className='pricingTable1 text-center'>
-                    <ul className='list-unstyled pricing-content-2 text-left py-3 border-0 mb-0'>
+                  <div className="pricingTable1 text-center">
+                    <ul className="list-unstyled pricing-content-2 text-left py-3 border-0 mb-0">
                       <li>
                         Please give NIU Health permission to access your camera
                         and microphone when starting the visit
