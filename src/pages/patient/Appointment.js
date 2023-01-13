fe160a782ca5337f4b4f3a2dd2de1e71d979efd6
Raseruuu <raseruuu@gmail.com>
2023-01-13 16:44:30 +0800

    Buttons at Patients' Appointments Page (#17)
    * Buttons at Patients' Appointments Page
    
    implemented Time details and Activating Button on schedule of appointment period.
    
    * Update .env

--- src/pages/patient/Appointment.js
+++ src/pages/patient/Appointment.js
@@ -4,12 +4,19 @@ import React, { useEffect,useState } from 'react'
 import { AWS_BUCKET } from '../../constants'
 import useAuth from '../../hooks/useAuth'
 import useAxiosPrivate from '../../hooks/useAxiosPrivate'
+import moment from "moment"
+import TableCard from "../../components/table/Tables"
+import CardItem from "../../components/cards/Card"
+function dateTimeFormat(date) {
+  return moment(date).format('MMM DD, YYYY, hh:mm A')
+}
+const timenow=moment()
 const CancelButton = () => {
 return(
   <div className="list-inline mb-0 align-self-center">
     <button
       type="button"
-      className="btn btn-danger btn-round waves-effect waves-light"
+      className="btn btn-outline-danger btn-round waves-effect waves-light"
     >
       Cancel Visit
     </button>
@@ -20,9 +27,21 @@ const ViewVisitButton = () => {
     <div className="list-inline mb-0 align-self-center">
       <button
         type="button"
-        className="btn btn-success btn-round waves-effect waves-light"
+        // className="btn btn-success btn-round waves-effect waves-light"
+        className="btn btn-outline-success btn-round waves-effect waves-light"
       >
-        View Visit Visit Summary
+        View Visit Summary
+      </button>
+    </div>
+  )}
+const StartButton = () => {
+  return(
+    <div className="list-inline mb-0 align-self-center">
+      <button
+        type="button"
+        className="btn btn-gradient-success btn-round waves-effect waves-light"
+      >
+        Join Virtual Visit
       </button>
     </div>
   )}
@@ -50,7 +69,7 @@ const StatusText = ({ status }) => {
     4: "badge-soft-success",
   }
   const statusText = {
-    0: 'Upcoming',
+    0: 'For Approval',
     2: 'Cancelled By You',
     3: 'Cancelled By Doctor',
     1: "Completed",
@@ -62,15 +81,67 @@ const StatusText = ({ status }) => {
     </span>
   )
 }
-const AppointmentAction = ({ status }) => {
+function HMFormat(minutes) {
+  
+  var hours = Math.floor(minutes / 60);
+  var days=Math.floor(hours / 24);
+  
+  // Getting the minutes.
+  var min = parseInt(minutes % 60);
+  // var sec= parseInt(min-60)
+  let dig=""
+  if (min<10){ dig="0"}
+  if (hours>24){
+    hours=hours-(days*24)
+    return days+" days"
+  }
+  else if(min>60){
+    return hours+" hrs and "+dig+min+" mins"
+  }
+  else{
+  return days+" days, "+ hours+" hrs and "+dig+min+" mins"}
+  // return hours+":"+dig+min+":"+sec
+}
+const AppointmentAction = ({ status , appointmentTime }) => {
+  const appointmentPeriod=[moment(appointmentTime),moment(appointmentTime).add(1, 'hours')]
+  const withinAppointmentPeriod=(timenow>appointmentPeriod[0]&&timenow<appointmentPeriod[1])
+  const appointmentETA=HMFormat(moment(appointmentTime).diff(timenow, 'minutes', true))
 
-  if (status==='upcoming'){
+  // const appointmentETAhrs=parseInt(moment(appointmentTime).diff(timenow, 'hours', true))
+  
+  
+  if (status==="4"&& withinAppointmentPeriod){
     return (
-      <CancelButton/>
+      <StartButton/>
     )}
-  else if (status==='completed'){
+  else if (status==="4"&& withinAppointmentPeriod){
+    return (
+      <StartButton/>
+    )}
+  else if (status==="4"&& !withinAppointmentPeriod&&timenow>appointmentPeriod[1]){
+      return (
+        <><h6>Virtual visit period is over.</h6>
+        <ViewVisitButton/>
+        </>
+      )}
+  else if (status==="4" && timenow<appointmentPeriod[1]){
     return (
       <>
+        {/* <h6>Appointment ETA: {appointmentETA}
+        </h6> */}
+        <CancelButton/>
+      </>
+    )}
+    
+  else if (status==="0"){
+    return (
+      
+      <CancelButton/>
+    )}
+  else if (status==="1"){
+    return (
+      <>
+
         <div className="br-wrapper br-theme-fontawesome-stars"><strong>Rate your experience: </strong> <select id="example-fontawesome" style={{display:"none"}}>
             <option value="1">1</option>
             <option value="2">2</option>
@@ -82,11 +153,30 @@ const AppointmentAction = ({ status }) => {
         <ViewVisitButton/>
       </>
     )}
+  // else{return (<>
+  //   {/* {"statusis4?"+(parseInt(status))+(status===4)} */}
+  //   <ViewVisitButton/>
+  //   </>
+    
+  // )}
+
 
 }
+function TimeCard(){
+    return(
+      <CardItem>
+        <h3>Time Now:</h3> 
+        <i className="far fa-fw fa-clock"></i>{dateTimeFormat(timenow)}
+      </CardItem>
+    )
+}
+function AppointmentItem({provider_description,provider_name,service_description,service_name,service_id,trans_start,trans_date_time,visit_id,status}){
+  
+  const dateTime=(trans_date_time+", "+trans_start+":00")
 
-function AppointmentItem({provider_description,provider_name,service_description,service_name,service_id,trans_date_time,visit_id,status}){
- 
+  const date=moment(trans_date_time).format('MMM DD, YYYY')
+  const time=moment(trans_date_time+", "+trans_start+":00").format('hh:mm A')+" - "+moment(dateTime).add(1, 'hours').format('hh:mm A')
+  // const time=moment(dateTime).get('hour')+":"+moment(dateTime).get('minute')
   return(
     <div className="card" id={service_id}>
     <div className="card-body">
@@ -96,12 +186,12 @@ function AppointmentItem({provider_description,provider_name,service_description
           {/* <i className="fas fa-circle text-purple"></i> */}
           <StatusIcon icontype={status}/>
         </div>
-
+        {/* {dateTimeFormat(trans_date_time+", "+trans_start+":00")} */}
         <p className="text-muted float-right">
-          <span className="text-muted">{trans_date_time}</span>
+          <span className="text-muted">{date}</span>
           <span className="mx-1">·</span>
           <span>
-            <i className="far fa-fw fa-clock"></i> {trans_date_time}
+            <i className="far fa-fw fa-clock"></i> {time}
             {/* //date */}
           </span>
         </p>
@@ -129,7 +219,7 @@ function AppointmentItem({provider_description,provider_name,service_description
           <div className="br-wrapper br-theme-fontawesome-stars">
             <strong>{provider_description}</strong> 
           </div>
-          <AppointmentAction status={status}/>
+          <AppointmentAction status={status} appointmentTime={dateTime} />
           
         </div>
       </div>
@@ -147,8 +237,21 @@ function Appointment() {
     visit_id:"",
     status:0
   }
+
   const [appointmentsList,setAppointmentsList] = useState([])
-   
+  const [sample_appointmentsList,setSample_AppointmentsList] = useState([
+    {provider_description:"Hi! I`m Jane Doe",
+      provider_name:"Jane Doe",
+      service_description: "service desc",
+      service_id: "16",
+      service_name: "Service Name",
+      status: "4",
+      trans_date_time: "2023-01-13",
+      trans_end:null,
+      trans_start:"13",
+      visit_id: "378"}
+  ])
+     
     const [errMsg, setErrMsg] = useState(null)
     const { auth } = useAuth()
     const axiosPrivate = useAxiosPrivate()
@@ -187,10 +290,12 @@ function Appointment() {
         controller.abort()
       }
     }, [])
+    var emptylist=[]
   return (
     <div className="page-wrapper">
       <div className="page-content">
         <div className="container-fluid">
+          
           <div className="row">
             <div className="col-sm-12">
               <div className="page-title-box">
@@ -198,14 +303,15 @@ function Appointment() {
               </div>
             </div>
           </div>
-
-          <div className="row">
+          <TimeCard/>
+          {(appointmentsList.length>0)?
+          (<div className="row">
             <div className="col-lg-12">
               {appointmentsList.map((appointment)=>
               <AppointmentItem {...appointment} />
               )}
             </div>
-          </div>
+          </div>):<CardItem><h4>There are no appointments to display.</h4></CardItem>}
         </div>
 
         <Footer />
